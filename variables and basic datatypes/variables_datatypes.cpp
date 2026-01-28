a6616138c0efb6261f7c930165d5f6468ec9e321
Leoo Tech's <246580573+LeooTech@users.noreply.github.com>
2026-01-28 22:06:38 +0000

    variables_datatypes1

--- variables and basic datatypes/variables_datatypes.cpp
+++ variables and basic datatypes/variables_datatypes.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 int main(){
-    int x;
-    x = 5;
-    std::cout << x;
+    int x = 5;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
 
     return 0;
 }
