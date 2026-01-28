ea661da330801ccfe0d5715fc2dd69779d6ad248
Leoo Tech's <246580573+LeooTech@users.noreply.github.com>
2026-01-28 23:50:23 +0000

    What are arithmetic operators?

---
+++ What are arithmetic operators/arithmeticop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//arithmetric operators = return the result of a specific arithmetic operation (+ - * /)
+
+int main(){
+
+    //int students = 20; // with [int] I wont get the decimals, so I would use double on that case.
+
+    //students++; //students+=1; //students = students + 1;
+    //students--; //students-=1; // students = students - 1;
+    //students*=2; // students = students * 2;
+    //students/=2; // students = students / 2;
+    //int remainder = students % 3   Divides the 20 students by groups of 3, but there would be a group of 2, so the remainder = 2
+
+    //parenthesis
+    //multiplication & division
+    //addition & subtraction
+    int students = 6 - 5 + 4 * 3 / 2;
+
+    std::cout << students;
+    
+    return 0;
+}
