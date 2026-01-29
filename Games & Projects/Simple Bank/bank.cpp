41e9a976d07e545ecd995fbc45ad697382bd288a
Leoo Tech's <246580573+LeooTech@users.noreply.github.com>
2026-01-29 18:22:05 +0000

    FIxing bank

--- Games & Projects/Simple Bank/bank.cpp
+++ Games & Projects/Simple Bank/bank.cpp
@@ -2,24 +2,31 @@
 
 int main(){
     
-    const double start_balance = 5400;
-    double balance;
+    const int start_balance = 5400;
+    char balance;
 
     std::cout << "Your current balance is: " << balance << "$";
     std::cout << "Choose what you would like to do next:" << '\n';
     std::cout << "[C]heck balance | [D]eposit money | [W]ithdraw money";
 
-    switch(balance){
+    switch(start_balance){
         case 'C':
             std::cout << "Your current balance is: " << balance << "$";
             break;
         case 'D':
+            double deposit;
+            balance = start_balance + deposit;
             std::cout << "You have successfuly added" << deposit << " to your balance!";
-            std::cout << "Your current balance is " << 
+            std::cout << "Your current balance is " << balance << "$";
             break;
         case 'W':
             double withdraw;
             balance = start_balance - withdraw;
-            std::cout << 
+            std::cout << "You have successfully withdrawn " << withdraw << "$ from your balance!";
+            std::cout << "Your current balance is " << balance << "$";
+            break;
+        default:
+            std::cout << "Please only enter in the provided options!";
+            break;
     }
 }
