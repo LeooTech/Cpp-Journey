14e3afc412eb7de4aad344008be340395704ba72
Leoo Tech's <246580573+LeooTech@users.noreply.github.com>
2026-01-29 00:17:52 +0000

    Math Related Functions

---
+++ Useful math related functions/math_func.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> // for pow/sqrt function and some other math functions
+
+int main(){
+
+    double x = 3.99;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y); Gets the bigger number between x, y 
+    //z = std::min(x, y); Gets the smallest number between x, y 
+    //z = pow(2, 4); //pow = expoente, ou seja 2^4 = 2x2x2x2 = 16
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x); Rounds the number removing decimal numbers.
+    //z = ceil(x); Gets a number above the current number if there is decimal numbers.
+    //z = floor(x) Rounds it down, basically we have 3.99 and instead of it being = 4, its = 3
+    std::cout << z;
+
+    return 0;
+}
